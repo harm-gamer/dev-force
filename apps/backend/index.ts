b2a8b450ab18ff1e795d78f5b9a6e08bd0007df7
mukund <mukundt513@gmail.com>
2025-10-20 22:21:42 +0530

    signup route

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,10 +1,14 @@
 import express from 'express'
+import user from "./routes/user.ts"
+import admin from "./routes/admin.ts"
+import context from "./routes/context.ts"
+
 
 const app = express();
 
-app.use("/user",userRouter);
-app.use("/admin",adminRouter);
-app.use("/context",contextRouter);
+app.use("/user",user);
+app.use("/admin",admin);
+app.use("/context",context);
 
 app.listen(3000,() =>{
     console.log("Server is running on port 3000");
--- apps/backend/routes/user.ts
+++ apps/backend/routes/user.ts
@@ -1,9 +1,43 @@
 import {Router} from "express";
 import {client} from "db/client"
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs"
+
+import { password } from "bun";
+
 const router = Router();
 
-router.post("/signup",(req,res) =>{
+const USER = {
+    username : String,
+    password : String
+}
 
+router.post("/signup", async (req,res) =>{
+   const {email} = req.body;
+
+  
+    // check if user exists
+    const existingUser = await client.user.findUnique({
+        where : {
+         email : email
+        }
+    })
+
+    if(existingUser){
+        return res.status(400).json({message : "User already exists"});
+    }
+
+    
+
+    
+
+    // crete user
+    const newUser = await client.user.create({
+        data : {
+            email : email || "" 
+        }
+    })
+    return res.status(201).json({user : newUser});
 })
 
 router.post("/login",(req,res) =>{
